80dd85c583858f3b7c4090d050add99109722089
Hiroaki Nakamura <hnakamur@gmail.com>
2009-09-12 10:57:16 +0900

    Major refactoring. Removed all static methods. svgdom is now an instance of Manipulator.

--- svgdom.js
+++ svgdom.js
@@ -34,54 +34,252 @@ var svgdom = (function() {
       $A = Array.prototype.slice;
 
   // SVG DOM Manipulator
-  function M(node) {
+  function Manipulator(node) {
     this.node = node;
   }
 
-  M.byId = function(id) {
-    return new M(document.getElementById(id));
-  };
+  function mixin(dest /* , sources */) {
+    for (var i = 1, n = arguments.length; i < n; i++) {
+      var src = arguments[i];
+      for (var k in src)
+        dest[k] = src[k];
+    }
+    return dest;
+  }
 
-  M.Fragment = function() {
+  mixin(Manipulator.prototype, {
+    mixin: mixin,
+    byId: byId,
+    Fragment: Fragment,
+    Element: Element,
+    TextNode: (isIE ? function TextNodeIE(text) {
+      // On Internet Explorer, DOM text nodes created through
+      // document.createTextNode with the second argument given as 'true':
+      //
+      // document.createTextNode('some text', true)
+      //
+      // will have a .style property on them as an artifact of how we support
+      // various things internally. Changing this will have no affect.
+      // Technically DOM text nodes should not have a .style property.
+      return new Manipulator(document.createTextNode(text, true));
+    } : function TextNode(text) {
+      return new Manipulator(document.createTextNode(text));
+    }),
+    append: append,
+    setAttr: setAttr,
+    setAttrNS: setAttrNS,
+    getAttr: getAttr,
+    getAttrNS: getAttrNS,
+
+    formatPath: formatPath,
+    coordSeparator: ",",
+
+    toFixed: toFixed,
+    toFixedCoord: toFixedCoord,
+    toFixedAngle: toFixedAngle,
+    coordFloatDigits: 1,
+    angleFloatDigits: 1,
+
+    deg2rad: deg2rad,
+    rad2deg: rad2deg,
+
+    curry: curry,
+    camelize: camelize,
+    hyphenize: hyphenize
+  });
+
+  function byId(id) {
+    var elem = document.getElementById(id);
+    return elem && new Manipulator(elem);
+  }
+  function Fragment() {
     // To create a DocumentFragment for use with SVG, you should call
     // document.createDocumentFragment(true). Note the extra true parameter --
     // this is required by SVG Web to help us know that this DocumentFragment
     // will be used with SVG, possibly going into our fake Flash backend.
-    return new M(document.createDocumentFragment(true));
-  };
+    return new Manipulator(document.createDocumentFragment(true));
+  }
+  function Element(type) {
+    return new Manipulator(document.createElementNS(svgns, type));
+  }
 
-  M.Element = function(type) {
-    return new M(document.createElementNS(svgns, type));
-  };
+  Manipulator.prototype.SVG = curry(Element, "SVG");
+  var types = [
+    "g", "defs", "desc", "title", "metadata", "symbol",
+    "use", "switch", "image", "style",
+    "path", "rect", "circle", "ellipse", "line", "polyline", "polygon",
+    "text", "tspan", "tref", "textPath",
+    "marker", "color-profile", "clipPath", "filter", "cursor",
+    "a", "view", "script",
+    "animate", "set", "animateMotion", "animateColor", "animateTransform",
+    "font", "glyph", "missing-glyph", "hkern", "vkern", "font-face",
+    "metadata"
+  ];
+  for (var i = 0, len = types.length; i < len; i++) {
+    var type = types[i];
+    Manipulator.prototype[camelize(type, true)] = curry(Element, type);
+  }
 
-  M.TextNode = isIE ? function(text) {
-    // On Internet Explorer, DOM text nodes created through
-    // document.createTextNode with the second argument given as 'true':
-    //
-    // document.createTextNode('some text', true)
-    //
-    // will have a .style property on them as an artifact of how we support
-    // various things internally. Changing this will have no affect.
-    // Technically DOM text nodes should not have a .style property.
-    return new M(document.createTextNode(text, true));
-  } : function(text) {
-    return new M(document.createTextNode(text));
-  };
+  function append() {
+    for (var i = 0, len = arguments.length; i < len; i++)
+      this.node.appendChild(arguments[i].node);
+    return this;
+  }
+  function setAttr(attributes) {
+    for (var k in attributes)
+      this.node.setAttribute(k, attributes[k]);
+    return this;
+  }
+  function setAttrNS(ns, attributes) {
+    for (var k in attributes)
+      this.node.setAttributeNS(ns, k, attributes[k]);
+    return this;
+  }
+  function getAttr(names) {
+    var ret = {};
+    for (var i = 0, len = names.length; i < len; i++) {
+      var name = names[i];
+      ret[name] = this.node.getAttribute(name);
+    }
+    return ret;
+  }
+  function getAttrNS(ns, names) {
+    var ret = {};
+    for (var i = 0, len = names.length; i < len; i++) {
+      var name = names[i];
+      ret[name] = this.node.getAttributeNS(ns, name);
+    }
+    return ret;
+  }
 
-  M.curry = function(fn) {
+  function toFixed(value, floatDigits) {
+    var s = value.toFixed(floatDigits);
+    var dotPos = s.indexOf(".");
+    if (dotPos == -1) return s;
+    for (var i = s.length - 1; i >= dotPos; i--) {
+      var c = s.charAt(i);
+      if (c != "0" && c != ".") break;
+    }
+    return s.substr(0, i + 1);
+  }
+  function toFixedCoord(value) {
+    return toFixed(value, this.coordFloatDigits);
+  }
+  function toFixedAngle(value) {
+    return toFixed(value, this.angleFloatDigits);
+  }
+
+  function formatPath(commands) {
+    var coordSeparator = this.coordSeparator,
+        s = [];
+    for (var i = 0, n = commands.length; i < n; i++) {
+      var command = commands[i],
+          cmdChar = command[0],
+          m = command.length,
+          j = 1,
+          paramCount = m - j;
+      s.push(cmdChar);
+      switch (cmdChar.toUpperCase()) {
+      case "M":
+      case "L":
+      case "T":
+        if (paramCount == 0 || paramCount % 2)
+          throw new Error("Parameter count should be 2 * n (n >= 1) for command ".concat(cmdChar, " but was ", paramCount));
+        while (j < m) {
+          if (j > 1) s.push(" ");
+          s.push(this.toFixedCoord(command[j++]));
+          s.push(coordSeparator);
+          s.push(this.toFixedCoord(command[j++]));
+        }
+        break;
+      case "S":
+      case "Q":
+        if (paramCount == 0 || paramCount % 4)
+          throw new Error("Parameter count should be 4 * n (n >= 1) for command ".concat(cmdChar, " but was ", paramCount));
+        while (j < m) {
+          if (j > 1) s.push(" ");
+          s.push(this.toFixedCoord(command[j++]));
+          s.push(coordSeparator);
+          s.push(this.toFixedCoord(command[j++]));
+          s.push(" ");
+          s.push(this.toFixedCoord(command[j++]));
+          s.push(coordSeparator);
+          s.push(this.toFixedCoord(command[j++]));
+        }
+        break;
+      case "C":
+        if (paramCount == 0 || paramCount % 6)
+          throw new Error("Parameter count should be 6 * n (n >= 1) for command ".concat(cmdChar, " but was ", paramCount));
+        while (j < m) {
+          if (j > 1) s.push(" ");
+          s.push(this.toFixedCoord(command[j++]));
+          s.push(coordSeparator);
+          s.push(this.toFixedCoord(command[j++]));
+          s.push(" ");
+          s.push(this.toFixedCoord(command[j++]));
+          s.push(coordSeparator);
+          s.push(this.toFixedCoord(command[j++]));
+          s.push(" ");
+          s.push(this.toFixedCoord(command[j++]));
+          s.push(coordSeparator);
+          s.push(this.toFixedCoord(command[j++]));
+        }
+        break;
+      case "H":
+      case "V":
+        if (paramCount == 0)
+          throw new Error("Parameter needed 0 for command ".concat(cmdChar));
+        while (j < m) {
+          if (j > 1) s.push(" ");
+          s.push(this.toFixedCoord(command[j++]));
+        }
+        break;
+      case "Z":
+        if (paramCount != 0)
+          throw new Error("Parameter count should be 0 for command ".concat(
+              cmdChar, " but was ", paramCount));
+        break;
+      case "A":
+        while (j < m) {
+          if (j > 1) s.push(" ");
+          s.push(this.toFixedCoord(command[j++]));
+          s.push(coordSeparator);
+          s.push(this.toFixedCoord(command[j++]));
+          s.push(" ");
+          s.push(this.toFixedAngle(command[j++]));
+          s.push(" ");
+          s.push(command[j++] ? 1 : 0);
+          s.push(" ");
+          s.push(command[j++] ? 1 : 0);
+          s.push(" ");
+          s.push(this.toFixedCoord(command[j++]));
+          s.push(coordSeparator);
+          s.push(this.toFixedCoord(command[j++]));
+        }
+        break;
+      default:
+        throw new Error("Unsupported path command. command=" + cmdChar);
+      }
+    }
+    return s.join("");
+  }
+
+  function deg2rad(degree) {
+    return Math.PI / 180 * degree;
+  }
+
+  function rad2deg(radian) {
+    return 180 / Math.PI * radian;
+  }
+
+  function curry(fn) {
     var _this = this, args = $A.call(arguments, 1);
     return function() {
       return fn.apply(_this, args.concat($A.call(arguments)));
     }
   }
 
-  M.mixin = function(dest, src) {
-    for (var k in src)
-      dest[k] = src[k];
-    return dest;
-  }
-
-  M.camelize = function(name, firstCapital) {
+  function camelize(name, firstCapital) {
     var chars = [],
         upperNext = firstCapital;
     for (var i = 0, len = name.length; i < len; i++) {
@@ -99,7 +297,7 @@ var svgdom = (function() {
     return chars.join("");
   }
 
-  M.hyphenize = function(name) {
+  function hyphenize(name) {
     var chars = [];
     for (var i = 0, len = name.length; i < len; i++) {
       var c = name.charAt(i);
@@ -113,170 +311,5 @@ var svgdom = (function() {
     return chars.join("");
   }
 
-  M.SVG = M.curry(M.Element, "SVG");
-  var types = [
-    "g", "defs", "desc", "title", "metadata", "symbol",
-    "use", "switch", "image", "style",
-    "path", "rect", "circle", "ellipse", "line", "polyline", "polygon",
-    "text", "tspan", "tref", "textPath",
-    "marker", "color-profile", "clipPath", "filter", "cursor",
-    "a", "view", "script",
-    "animate", "set", "animateMotion", "animateColor", "animateTransform",
-    "font", "glyph", "missing-glyph", "hkern", "vkern", "font-face",
-    "metadata"
-  ];
-  for (var i = 0, len = types.length; i < len; i++) {
-    var type = types[i];
-    M[M.camelize(type, true)] = M.curry(M.Element, type);
-  }
-
-  M.toFixed = function(value, floatDigits) {
-    var s = value.toFixed(floatDigits);
-    var dotPos = s.indexOf(".");
-    if (dotPos == -1) return s;
-    for (var i = s.length - 1; i >= dotPos; i--) {
-      var c = s.charAt(i);
-      if (c != "0" && c != ".") break;
-    }
-    return s.substr(0, i + 1);
-  }
-
-  M.formatPath = function(commands, options) {
-    var config = M.mixin(M.mixin({}, M.formatPath.defaults), options),
-        coordFloatDigits = config.coordFloatDigits,
-        angleFloatDigits = config.angleFloatDigits,
-        coordSeparator = config.coordSeparator,
-        s = [];
-    for (var i = 0, n = commands.length; i < n; i++) {
-      var command = commands[i],
-          cmdChar = command[0],
-          m = command.length,
-          j = 1,
-          paramCount = m - j;
-      s.push(cmdChar);
-      switch (cmdChar.toUpperCase()) {
-      case "M":
-      case "L":
-      case "T":
-        if (paramCount == 0 || paramCount % 2)
-          throw new Error("Parameter count should be 2 * n (n >= 1) for command ".concat(cmdChar, " but was ", paramCount));
-        while (j < m) {
-          if (j > 1) s.push(" ");
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-          s.push(coordSeparator);
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-        }
-        break;
-      case "S":
-      case "Q":
-        if (paramCount == 0 || paramCount % 4)
-          throw new Error("Parameter count should be 4 * n (n >= 1) for command ".concat(cmdChar, " but was ", paramCount));
-        while (j < m) {
-          if (j > 1) s.push(" ");
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-          s.push(coordSeparator);
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-          s.push(" ");
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-          s.push(coordSeparator);
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-        }
-        break;
-      case "C":
-        if (paramCount == 0 || paramCount % 6)
-          throw new Error("Parameter count should be 6 * n (n >= 1) for command ".concat(cmdChar, " but was ", paramCount));
-        while (j < m) {
-          if (j > 1) s.push(" ");
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-          s.push(coordSeparator);
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-          s.push(" ");
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-          s.push(coordSeparator);
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-          s.push(" ");
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-          s.push(coordSeparator);
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-        }
-        break;
-      case "H":
-      case "V":
-        if (paramCount == 0)
-          throw new Error("Parameter needed 0 for command ".concat(cmdChar));
-        while (j < m) {
-          if (j > 1) s.push(" ");
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-        }
-        break;
-      case "Z":
-        if (paramCount != 0)
-          throw new Error("Parameter count should be 0 for command ".concat(
-              cmdChar, " but was ", paramCount));
-        break;
-      case "A":
-        while (j < m) {
-          if (j > 1) s.push(" ");
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-          s.push(coordSeparator);
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-          s.push(" ");
-          s.push(M.toFixed(command[j++], angleFloatDigits));
-          s.push(" ");
-          s.push(command[j++] ? 1 : 0);
-          s.push(" ");
-          s.push(command[j++] ? 1 : 0);
-          s.push(" ");
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-          s.push(coordSeparator);
-          s.push(M.toFixed(command[j++], coordFloatDigits));
-        }
-        break;
-      default:
-        throw new Error("Unsupported path command. command=" + cmdChar);
-      }
-    }
-    return s.join("");
-  }
-  M.formatPath.defaults = {
-    coordFloatDigits: 1,
-    angleFloatDigits: 1,
-    coordSeparator: ","
-  }
-
-  M.mixin(M.prototype, {
-    append: function() {
-      for (var i = 0, len = arguments.length; i < len; i++)
-        this.node.appendChild(arguments[i].node);
-      return this;
-    },
-    setAttr: function(attributes) {
-      for (var k in attributes)
-        this.node.setAttribute(k, attributes[k]);
-      return this;
-    },
-    setAttrNS: function(ns, attributes) {
-      for (var k in attributes)
-        this.node.setAttributeNS(ns, k, attributes[k]);
-      return this;
-    },
-    getAttr: function(names) {
-      var ret = {};
-      for (var i = 0, len = names.length; i < len; i++) {
-        var name = names[i];
-        ret[name] = this.node.getAttribute(name);
-      }
-      return ret;
-    },
-    getAttrNS: function(ns, names) {
-      var ret = {};
-      for (var i = 0, len = names.length; i < len; i++) {
-        var name = names[i];
-        ret[name] = this.node.getAttributeNS(ns, name);
-      }
-      return ret;
-    }
-  });
-
-  return M;
+  return new Manipulator();
 })();
